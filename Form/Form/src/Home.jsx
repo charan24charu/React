925b996dd030eadebf83f8974f3dd8684d768ae3
charan G <charan.24.in@gmail.com>
2026-07-16 23:44:29 +0530

    Home

---
+++ Form/Form/src/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div id='home'>
+        <h1>Hi There, welcome to the Spaceeeee</h1>
+        <div id='home1'>
+            
+            
+        </div>
+    </div>
+  )
+}
+
+export default Home
--- Form/Form/src/main.jsx
+++ Form/Form/src/main.jsx
@@ -1,10 +1,11 @@
 
 import { createRoot } from 'react-dom/client'
-import Nav from './Nav.jsx'
+
 
 import  './App.css'
+import Home from './Home'
 
 createRoot(document.getElementById('root')).render(
-  <Nav></Nav>
-
+ 
+<Home></Home>
 )
