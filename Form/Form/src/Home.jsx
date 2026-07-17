125f8ddbd46462e8d66348110b11915c16a04061
charan G <charan.24.in@gmail.com>
2026-07-17 10:19:03 +0530

    login page

--- Form/Form/src/Home.jsx
+++ Form/Form/src/Home.jsx
@@ -4,9 +4,13 @@ const Home = () => {
   return (
     <div id='home'>
         <h1>Hi There, welcome to the Spaceeeee</h1>
+        <br />
         <div id='home1'>
-            
-            
+            <nav>
+           <button>Login</button>
+           
+           <button>SignUp</button>
+           </nav>
         </div>
     </div>
   )
